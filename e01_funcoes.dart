dd9971067fe248a448efeda9bc6ed5ba39b240de
Parisotto <parisotto@gmail.com>
2025-10-20 10:37:04 -0300

    primeiras aulas

---
+++ e01_funcoes.dart
@@ -0,0 +1,8 @@
+/**
+ * 1.Faça um programa que contenha uma função que receba uma lista de números inteiros e retorne outra função que retorne a soma dos números da lista recebida. 
+  
+ * 2. Faça uma função que receba dois números inteiros opcicionais sendo um deles com valor default 0 e retorne um boleano true se a soma dos dois números for par e false se for ímpar e também retorne um inteiro com a soma dos dois números e um texto informando se a soma é par ou ímpar.
+ 
+ * 3. Faça uma função que receba uma lista de nomes e retorne outra função do tipo arrow function que retorne a concatenação dos nomes da lista recebida em uma única string separada por vírgula.
+*/
+
